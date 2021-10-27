bc1da444c7e498510850485e31e8bc7604646f1f
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2021-10-27 16:33:13 -0400

    Initial Commit

---
+++ Shared/BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeApp.swift
+//  Shared
+//
+//  Created by Xiao Quan on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
